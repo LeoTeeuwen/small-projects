688f04909dfe22fdee1dee97094617475af523ff
Leo-Teeuwen <leoteeuwen325@gmail.com>
2026-05-28 21:04:38 -0500

    MLB API Call
     - I tried my best to do research and get the best we can from the API but I don't want to make an account and I can't get the full 2025 season so 2026 it is

--- baseball-database/pullDataAndPush.py
+++ baseball-database/pullDataAndPush.py
@@ -1,6 +1,10 @@
 # Goal of this file is to call the API for The Show and push all of that onto SQL
 
 import mysql.connector
+import requests
+import json
+
+mlb = mlbstatsapi.Mlb()
 
 mydb = mysql.connector.connect(
   host="127.0.0.1",
@@ -11,3 +15,10 @@ mydb = mysql.connector.connect(
 print(mydb)
 
 mydb.close()
+
+response = requests.get("https://statsapi.mlb.com/api/v1/teams?&hydrate=nextSchedule(team,gameType=[S,R,F,D,L,W,C],inclusive=false,limit=1)")
+
+data = response.json()  # Convert JSON response to a Python dictionary
+
+with open('data.json', 'w') as f:
+    json.dump(data, f, indent=4)
