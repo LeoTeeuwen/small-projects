e8ee06b3597ef94d26df55dc3709e23efcae0d20
Leo-Teeuwen <leoteeuwen325@gmail.com>
2026-05-29 19:24:19 -0500

    Getting all baseball data into dict

--- baseball-database/pullDataAndPush.py
+++ baseball-database/pullDataAndPush.py
@@ -18,5 +18,25 @@ response = requests.get("https://statsapi.mlb.com/api/v1/teams?&hydrate=nextSche
 
 data = response.json()  # Convert JSON response to a Python dictionary
 
-with open('data.json', 'w') as f:
-    json.dump(data, f, indent=4)
+# with open('data.json', 'w') as f:
+#     json.dump(data, f, indent=4)
+
+teamsData = []
+teamsDict = {}
+
+# print(data['teams'])
+# print(len(data['teams']))
+
+for team in data['teams']:
+  if("name" in team['league']  and (team['league']['name'] == "National League" or team['league']['name'] == "American League")):
+    # print(team['name'])
+    # print(team, end="\n\n")
+    print(team["nextGameSchedule"]["dates"][0]['games'][0]['teams']['away']['leagueRecord'], end="\n")
+    print(team["nextGameSchedule"]["dates"][0]['games'][0]['teams']['away']['team']['name'], end="\n")
+    print(team["nextGameSchedule"]["dates"][0]['games'][0]['teams']['home']['leagueRecord'], end="\n")
+    print(team["nextGameSchedule"]["dates"][0]['games'][0]['teams']['home']['team']['name'], end="\n\n")
+
+    teamsDict[team["nextGameSchedule"]["dates"][0]['games'][0]['teams']['away']['team']['name']] = team["nextGameSchedule"]["dates"][0]['games'][0]['teams']['away']['leagueRecord']
+    teamsDict[team["nextGameSchedule"]["dates"][0]['games'][0]['teams']['home']['team']['name']] = team["nextGameSchedule"]["dates"][0]['games'][0]['teams']['home']['leagueRecord']
+
+print(teamsDict)
