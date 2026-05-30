0e63900a909bfae57866bc0bc36d71f31f841e02
Leo-Teeuwen <leoteeuwen325@gmail.com>
2026-05-29 20:09:20 -0500

    Actually push the SQL data
     - Push the SQL data into the database now
     - TODO make sure the check for the table existing is better if I return to this

--- baseball-database/pullDataAndPush.py
+++ baseball-database/pullDataAndPush.py
@@ -2,41 +2,50 @@
 
 import mysql.connector
 import requests
-import json
 
 mydb = mysql.connector.connect(
   host="127.0.0.1",
   user="root",
-  password="password"
+  password="password",
+  database="the_show"
 )
-
-print(mydb)
-
-mydb.close()
 
 response = requests.get("https://statsapi.mlb.com/api/v1/teams?&hydrate=nextSchedule(team,gameType=[S,R,F,D,L,W,C],inclusive=false,limit=1)")
 
 data = response.json()  # Convert JSON response to a Python dictionary
 
-# with open('data.json', 'w') as f:
-#     json.dump(data, f, indent=4)
-
 teamsData = []
 teamsDict = {}
 
-# print(data['teams'])
-# print(len(data['teams']))
 
 for team in data['teams']:
   if("name" in team['league']  and (team['league']['name'] == "National League" or team['league']['name'] == "American League")):
-    # print(team['name'])
-    # print(team, end="\n\n")
-    print(team["nextGameSchedule"]["dates"][0]['games'][0]['teams']['away']['leagueRecord'], end="\n")
-    print(team["nextGameSchedule"]["dates"][0]['games'][0]['teams']['away']['team']['name'], end="\n")
-    print(team["nextGameSchedule"]["dates"][0]['games'][0]['teams']['home']['leagueRecord'], end="\n")
-    print(team["nextGameSchedule"]["dates"][0]['games'][0]['teams']['home']['team']['name'], end="\n\n")
 
     teamsDict[team["nextGameSchedule"]["dates"][0]['games'][0]['teams']['away']['team']['name']] = team["nextGameSchedule"]["dates"][0]['games'][0]['teams']['away']['leagueRecord']
     teamsDict[team["nextGameSchedule"]["dates"][0]['games'][0]['teams']['home']['team']['name']] = team["nextGameSchedule"]["dates"][0]['games'][0]['teams']['home']['leagueRecord']
 
-print(teamsDict)
+# print(teamsDict)
+
+myCursor = mydb.cursor()
+
+myCursor.execute("SHOW TABLES")
+
+
+# There is likely a more elegant way of doing this, but I do not want to wrap everything in a for loop so this is to escape that
+tableExists = False
+for x in myCursor:
+  if x[0] == 'teams':
+    tableExists = True
+
+if not tableExists:
+  myCursor.execute("CREATE TABLE teams (name VARCHAR(255) PRIMARY KEY, wins INT, losses INT, pct FLOAT)")
+
+myCursor.execute("TRUNCATE TABLE teams")
+
+for team in teamsDict:
+  myCursor.execute(f"INSERT INTO teams (name, wins, losses, pct) VALUES (%s, %s, %s, %s)", (team, teamsDict[team]['wins'], teamsDict[team]['losses'], teamsDict[team]['pct']))
+
+# Remember, you need to commit!!
+mydb.commit()
+
+mydb.close()
