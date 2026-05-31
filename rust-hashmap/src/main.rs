84ce42badd1fc08195dc8d200165b8c125a435ab
Leo-Teeuwen <leoteeuwen325@gmail.com>
2026-05-30 20:52:24 -0500

    made hashing function and search for function
     - TODO I realized I accidently neglected the actual value part of the key data pair, so that will be implemented soon

--- rust-hashmap/src/main.rs
+++ rust-hashmap/src/main.rs
@@ -1,8 +1,10 @@
 use core::num;
+use std::iter;
 
 pub struct HashMap<const u: usize> {
     pub size: usize,
     pub table: [String; u],
+    // TODO implement a new data pair attribute here
 }
 
 
@@ -12,7 +14,30 @@ impl<const u: usize> HashMap<u> {
         HashMap { size: u, table: [const {String::new()}; u] }
     }
 
-    // Read-only method (borrows self)
+    fn hash(&self, key: String) -> usize {
+
+        let mut hash: usize = 5381;
+
+        for i in key.chars() {
+            let c = i as usize;
+            hash = ((hash << 5) + hash) + c; 
+        }
+
+        return hash % self.size;
+    }
+
+    // TODO fix for data pair
+    pub fn insert(&mut self, key: &String) {
+        let arrayKey = self.hash(key.to_string());
+        self.table[arrayKey] = key.to_string();
+    }
+
+    // TODO fix for data pair
+    pub fn search(&self, key: String) -> String {
+        let arrayKey = self.hash(key);
+        return self.table[arrayKey].clone();
+    }
+
     pub fn print(&self) {
         println!("Starting!");
         for i in self.table.iter() {
@@ -23,6 +48,10 @@ impl<const u: usize> HashMap<u> {
 
 fn main() {
     println!("Hello, world!");
-    let hash:HashMap<20000> = HashMap::new();
-    hash.print();
+    let mut hash:HashMap<20000> = HashMap::new();
+    hash.insert(&("Hello!".to_string()));
+    let found = hash.search("Hello!".to_string());
+
+    println!("{found}")
+    
 }
