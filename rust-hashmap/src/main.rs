6880e374df25a931b80be2bd359abd66e094d66f
Leo-Teeuwen <leoteeuwen325@gmail.com>
2026-05-31 20:08:03 -0500

    Data array and improved search
     - Next is to work on porting this to a library in python

--- rust-hashmap/src/main.rs
+++ rust-hashmap/src/main.rs
@@ -1,17 +1,15 @@
-use core::num;
-use std::iter;
-
 pub struct HashMap<const u: usize> {
+    // Size is array index of key array
     pub size: usize,
-    pub table: [String; u],
-    // TODO implement a new data pair attribute here
+    pub keys: [String; u],
+    pub data: [String; u]
 }
 
 
 impl<const u: usize> HashMap<u> {
     // Constructor convention
     pub fn new() -> HashMap<u> {
-        HashMap { size: u, table: [const {String::new()}; u] }
+        HashMap { size: 0, keys: [const {String::new()}; u], data: [const {String::new()}; u] }
     }
 
     fn hash(&self, key: String) -> usize {
@@ -23,35 +21,35 @@ impl<const u: usize> HashMap<u> {
             hash = ((hash << 5) + hash) + c; 
         }
 
-        return hash % self.size;
+        return hash % u;
     }
 
     // TODO fix for data pair
-    pub fn insert(&mut self, key: &String) {
+    pub fn insert(&mut self, key: &String, value: &String) {
+        self.keys[self.size] = key.to_string();
         let arrayKey = self.hash(key.to_string());
-        self.table[arrayKey] = key.to_string();
+        self.data[arrayKey] = value.to_string();
     }
-
+    
     // TODO fix for data pair
     pub fn search(&self, key: String) -> String {
-        let arrayKey = self.hash(key);
-        return self.table[arrayKey].clone();
+        let arrayKey = self.hash(key.to_string());
+        return self.data[arrayKey].clone();
     }
 
     pub fn print(&self) {
         println!("Starting!");
-        for i in self.table.iter() {
-            println!("{i}");
+        for i in self.keys.iter() {
+            if i != "" {
+                println!("{i}");
+            }
         }
     }
 }
 
 fn main() {
     println!("Hello, world!");
-    let mut hash:HashMap<20000> = HashMap::new();
-    hash.insert(&("Hello!".to_string()));
-    let found = hash.search("Hello!".to_string());
-
-    println!("{found}")
-    
+    let mut hash:HashMap<10000> = HashMap::new();
+    hash.insert(&"Hello".to_string(), &"World".to_string());
+    let found = hash.search("Hello".to_string());    
 }
