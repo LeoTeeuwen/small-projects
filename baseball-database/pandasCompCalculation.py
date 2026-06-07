cc68bd157e96f0812bef5b0d82bdc0c0ac1bf0ae
Leo-Teeuwen <leoteeuwen325@gmail.com>
2026-06-06 19:51:48 -0500

    Prediction set up with basic example
     - Basic prediction set up for Padres and Dodgers

--- baseball-database/pandasCompCalculation.py
+++ baseball-database/pandasCompCalculation.py
@@ -5,6 +5,24 @@ from sqlalchemy import create_engine
 db_connection_str = 'mysql+pymysql://root:password@127.0.0.1/the_show'
 db_connection = create_engine(db_connection_str)
 
-dataframe = pd.read_sql('SELECT * FROM games', con=db_connection)
+df = pd.read_sql('SELECT * FROM games', con=db_connection)
 
-print(dataframe)
+print(df)
+
+
+# Figure out who is winning the Padres vs Dodgers matchup test
+result = df[df['away'].str.contains('Los Angeles Dodgers|San Diego Padres')][df['home'].str.contains('Los Angeles Dodgers|San Diego Padres')]
+
+print(result)
+
+dodgers_wins = df[df['away'].str.contains('Los Angeles Dodgers|San Diego Padres')][df['home'].str.contains('Los Angeles Dodgers|San Diego Padres')][df['winner'] == "Los Angeles Dodgers"]
+padres_wins = df[df['away'].str.contains('Los Angeles Dodgers|San Diego Padres')][df['home'].str.contains('Los Angeles Dodgers|San Diego Padres')][df['winner'] == "San Diego Padres"]
+
+print(len(result.index))
+print(dodgers_wins)
+print(padres_wins)
+
+if (len(dodgers_wins.index) > len(padres_wins.index)):
+    print("Dodgers Beat Padres!")
+else:
+    print("Padres Beat Dodgers!")
