3eda1455db5bffbf6280435b13b8b986cd194349
Leo-Teeuwen <leoteeuwen325@gmail.com>
2026-06-07 19:43:26 -0500

    Reformatted dataframe for no warnings
     - A Boolean Series error would occur upon running the code so it was re-formatted to remove this possbility

--- baseball-database/pandasCompCalculation.py
+++ baseball-database/pandasCompCalculation.py
@@ -5,16 +5,14 @@ from sqlalchemy import create_engine
 def simulateWinner(team1: str, team2: str, db_connection: any):
     df = pd.read_sql('SELECT * FROM games', con=db_connection)
 
-    print(df)
+    # Split up like this to not do querying and to avoid boolean series warning
+    team1_wins = df[df['away'].str.contains(f'{team1}|{team2}')]
+    team1_wins = team1_wins[team1_wins['home'].str.contains(f'{team1}|{team2}')]
+    team1_wins = team1_wins[team1_wins['winner'] == f"{team1}"]
 
-
-    # Figure out who is winning the Padres vs Dodgers matchup test
-    result = df[df['away'].str.contains(f'{team1}|{team2}')][df['home'].str.contains(f'{team1}|{team2}')]
-
-    print(result)
-
-    team1_wins = df[df['away'].str.contains(f'{team1}|{team2}')][df['home'].str.contains(f'{team1}|{team2}')][df['winner'] == f"{team1}"]
-    team2_wins = df[df['away'].str.contains(f'{team1}|{team2}')][df['home'].str.contains(f'{team1}|{team2}')][df['winner'] == f"{team2}"]
+    team2_wins = df[df['away'].str.contains(f'{team1}|{team2}')]
+    team2_wins = team2_wins[team2_wins['home'].str.contains(f'{team1}|{team2}')]
+    team2_wins = team2_wins[team2_wins['winner'] == f"{team2}"]
 
     if (len(team1_wins.index) > len(team2_wins.index)):
         return team1
@@ -22,7 +20,8 @@ def simulateWinner(team1: str, team2: str, db_connection: any):
     return team2
 
 
-db_connection_str = 'mysql+pymysql://root:password@127.0.0.1/the_show'
-db_connection = create_engine(db_connection_str)
+if __name__ == '__main__':
+    db_connection_str = 'mysql+pymysql://root:password@127.0.0.1/the_show'
+    db_connection = create_engine(db_connection_str)
 
-print(simulateWinner("Los Angeles Dodgers", "San Diego Padres", db_connection))
+    print(simulateWinner("Los Angeles Dodgers", "San Diego Padres", db_connection))
